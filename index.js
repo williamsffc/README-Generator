854e56cd30b2dae0d910948fd8d9d9f459f0fc69
William Flores <williamsffc@gmail.com>
2020-08-17 16:30:16 -0700

    more updates

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const inquirer = require("inquirer");
 
 
 // array of questions for user
-const questions = [
-    {
-        name: "title",
-        message: "What is your project's title?",
-    },
-];
+// const questions = [
+//     {
+//         name: "title",
+//         message: "What is your project's title?",
+//     },
+// ];
 
 inquirer
     .prompt([
@@ -65,14 +65,15 @@ inquirer
         console.log("Answer: ", answers);
     });
 
-// function to write README file
-// function writeToFile(fileName, data) {
-// }
 
-// function to initialize program
+// function to write README file:
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program:
 // function init() {
 
 // }
 
-// function call to initialize program
+// function call to initialize program:
 // init();
